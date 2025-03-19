46103a67b5f013fe93aaaa46074eba2dd5b653bc
Dinis Mubai <mubydinis@gmail.com>
2025-03-19 15:22:23 +0200

    Week03 Learning activities

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /*****************************
@@ -29,9 +31,11 @@ app.use(static)
 /* ***********************
  * Index Route
  *************************/
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 /* ************************************
  * Local Server Information
